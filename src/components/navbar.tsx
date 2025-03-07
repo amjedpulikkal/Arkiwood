5c943fd7ba3106960e008e7e6572a6a0abb1198a
amjedpulikkal <amjed0pulikkal@gmail.com>
2025-03-07 15:07:13 +0530

    bug fixed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,38 +1,43 @@
-
-
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { DrawerNav } from "./navDrawer";
 export default function Navbar() {
   return (
-    <nav className=" h-16 md:h-20 flex justify-between items-center px-4  " >
-      <div className="sm;p-0 p-8"  >
+    <nav className=" h-16 md:h-20 flex justify-between items-center px-4  ">
+      <div className=" ">
         <Link href={"/"}>
-        <Image
-          className="dark:invert"
-          src="/logo.png"
-          alt="logo"
-          width={140}
-          height={100}
-          priority
-        />
+          <Image
+            className="w-28 md:w-36 lg:w-40 dark:invert"
+            src="/logo.png"
+            alt="logo"
+            width={140}
+            height={100}
+            priority
+          />
         </Link>
       </div>
 
       <div className="sm:hidden">
-      <DrawerNav/>
-      
+        <DrawerNav />
       </div>
       <div className="w-1/2 text-xl nasalization hidden sm:block text-[#7f6456d7]">
-        <ul className="flex justify-around ">
-          <Link href={"#our-services"}>
-          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">Services</li>
-          </Link>
-          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">Blogs</li>
-          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">Projects</li>
-          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">About</li>
-          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">Contact</li>
+        <ul className="flex justify-between md:justify-around gap-x-6">
+          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">
+            <Link href={"#our-services"}>Services</Link>
+          </li>
+          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">
+            <Link href={"#blogs"}>Blogs</Link>
+          </li>
+          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">
+            <Link href={"#projects"}>Projects</Link>
+          </li>
+          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">
+            <Link href={"#about"}>About</Link>
+          </li>
+          <li className="hover-underline-animation center hover:text-[#7F6456] transition-colors">
+            <Link href={"#contact"}>Contact</Link>
+          </li>
         </ul>
       </div>
     </nav>
